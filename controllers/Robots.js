00121c2d7535131217891883f308e71bf688faa7
Rishil-NWM <156384714+Rishil-NWM@users.noreply.github.com>
2024-04-10 15:32:33 -0500

    first commit

--- controllers/Robots.js
+++ controllers/Robots.js
@@ -1,61 +1,80 @@
-var Robots= require('../models/Robots');
-// List of all food
+var Robots = require('../models/Robots');
+// List of all Robots
 exports.Robots_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Robots list');
+ res.send('NOT IMPLEMENTED: Robots list');
 };
-// for a specific Dog.
-exports.Robots_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Robots detail: ' + req.params.id);
+// for a specific Robots.
+exports.Robots_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await Robots.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
 };
-// Handle Dog create on POST.
+
+// Handle Robots create on POST.
 exports.Robots_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Robots create POST');
+ res.send('NOT IMPLEMENTED: Robots create POST');
 };
-// Handle Dog delete from on DELETE.
+// Handle Robots delete from on DELETE.
 exports.Robots_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Robots delete DELETE ' + req.params.id);
+ res.send('NOT IMPLEMENTED: Robots delete DELETE ' + req.params.id);
 };
-// Handle Dog update form on PUT.
-exports.Robots_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Robots update PUT' + req.params.id);
+// Handle Robots update form on PUT.
+exports.Robots_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Costume.findById( req.params.id)
+// Do updates of properties
+if(req.body.Robot_type)
+toUpdate.Robot_type = req.body.Robot_type;
+if(req.body.price) toUpdate.cost = req.body.price;
+if(req.body.manufactured) toUpdate.size = req.body.manufactured;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
- 
- 
+
+
 exports.Robots_list = async function(req, res) {
     try{
-    theRobots = await Robots.find();
-    res.send(theRobots);
+    thefood = await Robots.find();
+    res.send(thefood);
     }
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
     }
    };
- 
-exports.Robots_view_all_Page = async function(req, res) {
+ exports.Robots_view_all_Page = async function(req, res) {
     try{
-    theRobotss = await Robots.find();
-    res.render('Robots', { title: 'Robots Search Results', results: theRobots });
+    thefoods = await Robots.find();
+    res.render('Robots', { title: 'Robots Search Results', results: thefoods });
     }
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
     }
     };
- 
- 
-    // Handle food create on POST.
+// Handle Robots create on POST.
 exports.Robots_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Robots();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    document.Robot_type = req.body.Robot_type;
+    document.Robot_type = req.body. Robot_type;
     document.price = req.body.price;
     document.manufactured = req.body.manufactured;
-    
- 
     try{
     let result = await document.save();
     res.send(result);
@@ -65,3 +84,4 @@ exports.Robots_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+    

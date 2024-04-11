4815f00b29e2114b474100f5e662eff1f38a6f43
Rishil-NWM <156384714+Rishil-NWM@users.noreply.github.com>
2024-04-10 23:13:32 -0500

    first change

--- routes/Robots.js
+++ routes/Robots.js
@@ -1,23 +1,23 @@
 var express = require('express');
-const Robots_controllers= require('../controllers/food');
+const Robots_controllers= require('../controllers/Robots');
 var router = express.Router();
-/* GET food */
+/* GET Robots */
 
-router.get('/', Robots_controllers.food_view_all_Page);
+router.get('/', Robots_controllers.Robots_view_all_Page);
 
 router.put('/', function(req, res) {
     if(req.body.checkboxsale)toUpdate.sale = true;
     else toUpdate.same = false;
     })
-router.get('/', Robots_controllers.food_delete );
-/* GET detail food page */
-router.get('/detail', Robots_controllers.food_view_one_Page);
-/* GET create food page */
-router.get('/create', Robots_controllers.food_create_Page);
+router.get('/', Robots_controllers.Robots_delete );
+/* GET detail Robots page */
+router.get('/detail', Robots_controllers.Robots_view_one_Page);
+/* GET create Robots page */
+router.get('/create', Robots_controllers.Robots_create_Page);
 /* GET create update page */
-router.get('/update', Robots_controllers.food_update_Page);
-/* GET delete food page */
-router.get('/delete', Robots_controllers.food_delete_Page);
+router.get('/update', Robots_controllers.Robots_update_Page);
+/* GET delete Robots page */
+router.get('/delete', Robots_controllers.Robots_delete_Page);
 
 
 module.exports = router;

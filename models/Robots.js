29513a471eac7d17f99ba731ab3faf2769527362
Rishil-NWM <156384714+Rishil-NWM@users.noreply.github.com>
2024-04-17 16:42:32 -0500

    chnaged

--- models/Robots.js
+++ models/Robots.js
@@ -1,7 +1,9 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const RobotsSchema = mongoose.Schema({
-Robot_type: String,
-price: Number,
-manufactured: String
+    Robot_type: String,
+    price: { type: Number, min: 1000, max: 200000 },
+    manufactured: String
 });
-module.exports = mongoose.model("Robots",RobotsSchema)
+
+module.exports = mongoose.model("Robots", RobotsSchema);

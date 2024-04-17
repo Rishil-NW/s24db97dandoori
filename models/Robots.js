59fa95b76afb12caf89da865e13ab6632a3fe824
Rishil-NWM <156384714+Rishil-NWM@users.noreply.github.com>
2024-04-17 16:58:52 -0500

    first

--- models/Robots.js
+++ models/Robots.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const RobotsSchema = mongoose.Schema({
     Robot_type: String,
     price: { type: Number, min: 1000, max: 200000 },
-    manufactured: String
+    manufactured: { type: String, minlenght: 10, maxlength: 1234 }
 });
 
 module.exports = mongoose.model("Robots", RobotsSchema);
